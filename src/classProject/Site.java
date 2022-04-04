ac8511f067e14161a5a73d791e486aa631a5c513
ayeonggg <ayeong2021011301@gnu.ac.kr>
2022-04-04 22:23:12 +0900

    site

--- src/classProject/Site.java
+++ src/classProject/Site.java
@@ -1,24 +1,25 @@
+package classProject;
 
-import java.util.Scanner;
 public class Site {
+	
 	String name;
 	String address;
 	String folder;
 	
 	public Site() {
-//		numSitesRegistered++;
 	}
+	
 	public Site(String name, String address) {
 		this.name= name;
 		this.address= address;
-//		numSitesRegistered++;
+
 	}
 	public Site(String name, String address, String folder) {
 		this.name= name;
 		this.address=address;
 		this.folder= folder;
-//		numSitesRegistered++;
 	}
+	
 	public void printInfo() {
 		System.out.println("name: "+name+ "address: "+address+ "folder: "+folder);
 	}

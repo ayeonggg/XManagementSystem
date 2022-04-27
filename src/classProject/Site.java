7374b38c7ef39e1d89436031b054b2c4eb31dadc
ayeonggg <ayeong2021011301@gnu.ac.kr>
2022-04-27 16:54:05 +0900

    replay

--- src/classProject/Site.java
+++ src/classProject/Site.java
@@ -1,10 +1,11 @@
 package classProject;
 
+import java.util.Scanner;
+
 public class Site {
-	
-	String name;
-	String address;
-	String folder;
+	protected String name;
+	protected String address;
+	protected String folder;
 	
 	public Site() {
 	}
@@ -20,8 +21,56 @@ public class Site {
 		this.folder= folder;
 	}
 	
+	protected SiteKind kind= SiteKind.Chrome;
+	public SiteKind getKind() {
+		return kind;
+	}
+
+	public void setKind(SiteKind kind) {
+		this.kind = kind;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getFolder() {
+		return folder;
+	}
+
+	public void setFolder(String folder) {
+		this.folder = folder;
+	}
+
+	
 	public void printInfo() {
 		System.out.println("name: "+name+ "address: "+address+ "folder: "+folder);
+	}
+	
+	public void getUserInput(Scanner input) {
+		System.out.print("Site Address: ");
+		String address= input.next();
+		this.setAddress(address);
+		
+		System.out.print("Site Name: ");
+		String name= input.next();
+		this.setName(name);
+		
+		System.out.print("Folder: ");
+		String folder= input.next();
+		this.setFolder(folder);
 	}
 }
 	
--- src/classProject/SiteManager.java
+++ src/classProject/SiteManager.java
@@ -1,53 +1,114 @@
 package classProject;
 
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SiteManager {
-	Site site;
+	ArrayList<Site> sites= new ArrayList<Site>();
 	Scanner input;
 	SiteManager(Scanner input){
 		this.input= input;
 	}
 	
 	public void addsite() {
-		site= new Site();
-		System.out.print("Site Address: ");
-		site.address= input.next();
-		System.out.print("Site Name: ");
-		site.name= input.next();
-		System.out.print("Folder: ");
-		site.folder= input.next();
+		int kind=0;
+		Site site;
+		while (kind !=1 && kind !=2) {
+			System.out.print("1 for Chorm: ");
+			System.out.print("2 for Microsoft Edge: ");
+			System.out.print("Select num for Site Kind between 1 and 2: ");
+		    kind= input.nextInt();
+			if (kind==1) {
+			    site= new Site();
+			    site.getUserInput(input);
+			    sites.add(site);
+			    break;
+			}
+			else if (kind ==2) {
+				site= new MicrosoftEdgeSite();
+				 site.getUserInput(input);
+				sites.add(site);
+				break;
+			}
+			else {
+				System.out.println("Select num for Site Kind between 1 and 2");
+			}
 		}
+	}
+	
 	
 public void deletesite() {
 		System.out.print("Site Address: ");
 		String siteaddress=input.next();
-		if(site ==null) {
+		int index=-1;
+		for(int i=0; i<sites.size(); i++) {
+			if(sites.get(i).address==siteaddress) {
+				index= i;
+				break;
+			}
+		}
+		if(index>=0) {
+			sites.remove(index);
+			System.out.println("the site" +siteaddress+ "is deleted.");
+		}
+		else {
 			System.out.println("the site has not been registered.");
 			return;
+		
 		}
-
-		if(site.address.equals (siteaddress)) {
-			site= null;
-			System.out.println("the site is deleted.");
-		}
-	}
+	} 
 	
 	public  void editsite() {
 		System.out.print("Site Address: ");
 		String siteaddress= input.next();
-		if(site.address.equals (siteaddress)) {
-			System.out.println("the site to be edited is"+ siteaddress);
+		for(int i=0; i<sites.size(); i++) {
+			Site site=sites.get(i);
+			if(site.address.equals (siteaddress)) {
+				int num=-1;
+				while(num!=5){
+					System.out.println("*** Student Mangement System Menu ***");
+					System.out.println("1. Edit Address");
+					System.out.println("2. Edit Name");
+					System.out.println("3. Edit folder");
+					System.out.println("4. View Sites");
+					System.out.println("5. Exit");
+					System.out.println("Select one number between 1-5:");
+					num=input.nextInt();
+					if(num==1) {
+						System.out.print("Site address:");
+						String address=input.next();
+						site.setAddress(address);
+					}
+					else if(num==2) {
+						System.out.print("Site name:");
+						String name=input.next();
+						site.setName(name);
+					}
+					else if(num==3) {
+						System.out.print("Site folder:");
+						String folder=input.next(); 
+						site.setFolder(folder);
+					}
+					else {
+						continue;
+					}
+				}
+				break;
+			}
 		}
 	}
-	
-	public void viewsite() {
-		System.out.print("Site Address: ");
-		String siteaddress= input.next();
-		if(site.address.equals (siteaddress)) {
-			site.printInfo();
+
+	public void viewsites() {
+//		System.out.print("Site Address: ");
+//		String siteaddress= input.next();
+		System.out.println("# of registered sites: "+ sites.size());
+		for(int i=0; i<sites.size(); i++) {
+			sites.get(i).printInfo();
 		}
+//		if(site.address.equals (siteaddress)) {
+//			site.printInfo();
+//		}
 	}
 }
 	
